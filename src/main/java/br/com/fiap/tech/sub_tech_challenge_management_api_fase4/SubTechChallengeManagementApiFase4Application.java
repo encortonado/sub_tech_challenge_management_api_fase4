4a5976a61a65242ac56fa26d5a95d2e0954859be
Lucas Bortolo <lucasbortolo3@gmail.com>
2024-10-19 10:35:54 -0300

    first commit

---
+++ src/main/java/br/com/fiap/tech/sub_tech_challenge_management_api_fase4/SubTechChallengeManagementApiFase4Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.sub_tech_challenge_management_api_fase4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubTechChallengeManagementApiFase4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubTechChallengeManagementApiFase4Application.class, args);
+    }
+
+}
